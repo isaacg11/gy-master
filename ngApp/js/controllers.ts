9dc8e783481f6abd8cef067cfab79ce75c7a1c3d
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-09-19 16:30:07 -0700

    added more

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,4 +1,36 @@
+// dropdown menu
+let j;
 
+j('[data-toggle="popover"]').popover({html:true});
+
+// typed.js
+// $(function(){
+//   $(".element").typed({
+//     strings: [
+//       "garden ^1500",
+//       "micro-farm ^1500",
+//       "money maker ^1500"
+//     ],
+//     typeSpeed: 5,
+//     loop: true
+//   });
+// });
+
+// smooth scroll
+// $(document).ready(function(){
+//   $('a[href^="#"]').on('click',function (e) {
+//     e.preventDefault();
+//
+//     var target = this.hash;
+//     var $target = $(target);
+//
+//     $('html, body').stop().animate({
+//       'scrollTop': $target.offset().top
+//     }, 1000, 'swing', function () {
+//       window.location.hash = target;
+//     });
+//   });
+// });
 
 namespace app.Controllers {
   export class HomeController {
