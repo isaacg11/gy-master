c6fc8af61bc6e6fba7b4fb694d1c2f0d04bc8fa7
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-09-20 14:39:58 -0700

    testing

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -1,8 +1,12 @@
 
 namespace app.Controllers {
   export class HomeController {
+    public options;
+
     constructor(
+      $sce: ng.ISCEService
     ) {
+      this.options = $sce.trustAsHtml('Line 1<br>Line2');
     }
   }
 
