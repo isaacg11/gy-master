446237fffcba849277df360040eba782f6f85834
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-09-27 14:43:58 -0700

    made some UI changes

--- api/yarden.ts
+++ api/yarden.ts
@@ -3,11 +3,6 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let sg = require('sendgrid')('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');
 let helper = require('sendgrid').mail;
-let from_email = new helper.Email('noreply@yarden.garden');
-let to_email = new helper.Email('isaac.j.grey@gmail.com');
-let subject = 'Hello World from the SendGrid Node.js Library!';
-let content = new helper.Content('text/plain', 'Hello, Email!');
-let mail = new helper.Mail(from_email, subject, to_email, content);
 
 // models
 let Customer = mongoose.model('Customer', {
@@ -24,30 +19,35 @@ let Application = mongoose.model('Application', {
 
 // POST - new customer
 router.post('/customer', function(req, res){
-  let request = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: mail.toJSON(),
+  let newCustomer = new Customer({
+    email: req.body.email
   });
 
-  sg.API(request, function(error, response) {
-    if(error) {
-      res.send(error);
+  newCustomer.save((err, customer) => {
+    if(err) {
+      res.send(err);
     } else {
-      res.send(response);
+      let from_email = new helper.Email('noreply@yarden.garden');
+      let to_email = new helper.Email(req.body.email);
+      let subject = "Registration confirmed for Yarden!";
+      let content = new helper.Content('text/plain', "Ahoy! we've added you to the waiting list for Yarden BETA, you will be notified by when it is released to the public. Cheers!");
+      let mail = new helper.Mail(from_email, subject, to_email, content);
+
+      let request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+      });
+
+      sg.API(request, function(error, response) {
+        if(error) {
+          res.send(error);
+        } else {
+          res.send(response);
+        }
+      });
     }
-  });
-  // let newCustomer = new Customer({
-  //   email: req.body.email
-  // });
-  //
-  // newCustomer.save((err, customer) => {
-  //   if(err) {
-  //     res.send(err);
-  //   } else {
-  //
-  //   }
-  // })
+  })
 });
 
 // POST - new application
