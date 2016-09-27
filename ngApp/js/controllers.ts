f1f16c1650dfa3307b4b8eec35feb72339683ccf
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-09-26 22:04:39 -0700

    made some UI changes

--- ngApp/js/controllers.ts
+++ ngApp/js/controllers.ts
@@ -10,19 +10,7 @@ namespace app.Controllers {
 
     public submit() {
       // this.yardenService.save(this.email).then(() => {
-        this.$scope.alerts = [
-          { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
-          { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
-        ];
 
-        this.$scope.addAlert = function() {
-          this.$scope.alerts.push({msg: 'Another alert!'});
-        };
-
-        this.$scope.closeAlert = function(index) {
-          this.$scope.alerts.splice(index, 1);
-        };
-      // })
     }
 
     constructor(
