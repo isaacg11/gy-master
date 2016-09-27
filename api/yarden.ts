ae09bde182edf58c75053da17968d7243db1b539
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-09-26 20:47:11 -0700

    made some UI changes

--- api/yarden.ts
+++ api/yarden.ts
@@ -2,7 +2,7 @@ import express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-router.post('/api/customer', function(req, res){
+router.post('/customer', function(req, res){
   console.log(req.body);
   res.send({message: 'success'});
 });
