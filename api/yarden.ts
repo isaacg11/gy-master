639560ca2993258e0dcd50635a0da8c1e24370e3
isaacg11 <isaacgreywebdeveloper@gmail.com>
2016-09-26 21:10:16 -0700

    made some UI changes

--- api/yarden.ts
+++ api/yarden.ts
@@ -2,9 +2,24 @@ import express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
+// model
+
+let Customer = mongoose.model('Customer', {
+  email: String
+})
+
 router.post('/customer', function(req, res){
-  console.log(req.body);
-  res.send({message: 'success'});
+  let newCustomer = new Customer({
+    email: req.body.email
+  });
+
+  newCustomer.save((err, customer) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(customer);
+    }
+  })
 });
 
 export = router;
